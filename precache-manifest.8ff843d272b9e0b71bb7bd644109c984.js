87fb34bf65dfee18ed463672227fa30aeef4e98f
Gan <jgan6184@floridapoly.edu>
2020-04-28 09:55:30 -0400

    Updates

---
+++ precache-manifest.8ff843d272b9e0b71bb7bd644109c984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698d7d0d045578908915714cf3ac8d92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80e8ab02d2f2cd7d63ae",
+    "url": "/static/css/main.abc8aeea.chunk.css"
+  },
+  {
+    "revision": "7c3386624756ba5dc253",
+    "url": "/static/js/2.898115ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.898115ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e8ab02d2f2cd7d63ae",
+    "url": "/static/js/main.9857bdc4.chunk.js"
+  },
+  {
+    "revision": "9992f503cd6211a993cb",
+    "url": "/static/js/runtime-main.77ad2b87.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TodoList/precache-manifest.fae4be436ee9b9a4339db2df6ca1e4b3.js"
+  "/precache-manifest.8ff843d272b9e0b71bb7bd644109c984.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TodoList/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
