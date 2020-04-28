f3bdd3f98de5871f10598e92d79778ef8c114379
Gan <jgan6184@floridapoly.edu>
2020-04-28 01:03:29 -0400

    testing heroku

---
+++ build/precache-manifest.fae4be436ee9b9a4339db2df6ca1e4b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0872c13f6df4472f39b37008f00111b0",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "644c47fd7e65cf5d9f3b",
+    "url": "/TodoList/static/css/main.abc8aeea.chunk.css"
+  },
+  {
+    "revision": "7c3386624756ba5dc253",
+    "url": "/TodoList/static/js/2.898115ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.898115ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644c47fd7e65cf5d9f3b",
+    "url": "/TodoList/static/js/main.9a78505a.chunk.js"
+  },
+  {
+    "revision": "36d1ce1ed2a2992b2b33",
+    "url": "/TodoList/static/js/runtime-main.db4975af.js"
+  }
+]);
